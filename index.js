47455e09e65d76274f5f297b367632a039ab7c38
sammy <44491450+samuelkanyingi@users.noreply.github.com>
2024-11-26 09:20:57 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let clickedValues = ['1', '+', '1']
-let filtered = clickedValues.filter((arr)=> {
-    return    arr  != '+'
-}) 
-const sum = filtered.reduce((total, currVal) => total + Number(currVal),0)
-
-console.log(sum)
